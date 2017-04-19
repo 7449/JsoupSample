3ccb0717a4787bdee587c15bb28414b9d64e4a33
@ <yuezhaoyang7449@163.com>
2017-04-19 18:22:42 +0800

    add search

--- image/src/main/java/com/image/mian/widget/MainActivity.java
+++ image/src/main/java/com/image/mian/widget/MainActivity.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.framework.base.BaseActivity;
+import com.framework.utils.UIUtils;
 import com.image.R;
 import com.image.collection.list.CollectionListFragment;
 import com.image.manager.ApiConfig;
@@ -17,9 +20,11 @@ import com.image.mian.presenter.MainPresenter;
 import com.image.mian.presenter.MainPresenterImpl;
 import com.image.mian.view.MainView;
 import com.rxjsoupnetwork.manager.RxJsoupDisposeManager;
+import com.search.SearchFragment;
+import com.search.SearchToActivityInterface;
 
 public class MainActivity extends BaseActivity
-        implements NavigationView.OnNavigationItemSelectedListener, MainView {
+        implements NavigationView.OnNavigationItemSelectedListener, MainView, SearchToActivityInterface {
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
@@ -34,6 +39,7 @@ public class MainActivity extends BaseActivity
         presenter = new MainPresenterImpl(this);
         navigationView.setNavigationItemSelectedListener(this);
         toolbar.setTitle(getString(R.string.dbmz_title));
+        setSupportActionBar(toolbar);
         switchDouban();
     }
 
@@ -47,11 +53,17 @@ public class MainActivity extends BaseActivity
 
     @Override
     public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        SearchFragment searchFragment = (SearchFragment) fragmentManager.findFragmentByTag(SearchFragment.SEARCH_TAG);
+        if (searchFragment != null) {
+            searchFragment.onBack();
         } else {
-            RxJsoupDisposeManager.getInstance().clearDispose();
-            super.onBackPressed();
+            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.closeDrawer(GravityCompat.START);
+            } else {
+                RxJsoupDisposeManager.getInstance().clearDispose();
+                super.onBackPressed();
+            }
         }
     }
 
@@ -72,6 +84,22 @@ public class MainActivity extends BaseActivity
         }
         presenter.switchId(item.getItemId());
         drawerLayout.closeDrawers();
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                SearchFragment.startFragment(this, this);
+                break;
+        }
         return true;
     }
 
@@ -105,4 +133,13 @@ public class MainActivity extends BaseActivity
         replaceFragment(R.id.fragment, CollectionListFragment.newInstance());
     }
 
+    @Override
+    public void onSearchStart(String content) {
+
+    }
+
+    @Override
+    public void emptySearch() {
+        UIUtils.toast(UIUtils.getString(R.string.search_empty));
+    }
 }
